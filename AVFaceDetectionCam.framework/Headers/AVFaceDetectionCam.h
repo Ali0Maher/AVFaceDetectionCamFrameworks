6a5c8c0170f8d29bc1f5c55375e058478a22b60a
Ali <Ali.maheir@gmail.com>
2021-08-26 22:34:18 +0200

    v1.1.1

---
+++ AVFaceDetectionCam.framework/Headers/AVFaceDetectionCam.h
@@ -0,0 +1,18 @@
+//
+//  AVFaceDetectionCam.h
+//  AVFaceDetectionCam
+//
+//  Created by Ali on 8/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AVFaceDetectionCam.
+FOUNDATION_EXPORT double AVFaceDetectionCamVersionNumber;
+
+//! Project version string for AVFaceDetectionCam.
+FOUNDATION_EXPORT const unsigned char AVFaceDetectionCamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVFaceDetectionCam/PublicHeader.h>
+
+
